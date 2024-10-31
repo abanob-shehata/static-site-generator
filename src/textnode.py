b442d75a77fcbdccacbee019aa68e5384b49502d
abanob-shehata <abanob.sheh@gmail.com>
2024-10-31 14:09:27 +0900

    add textnode to htmlnode coversion

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     TEXT = "text"
@@ -23,3 +24,22 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+def text_node_to_html_node(text_node : TextNode):
+    match (text_node.text_type):
+        case (TextType.TEXT.value):
+            return LeafNode(None, text_node.text)
+        case (TextType.BOLD.value):
+            return LeafNode("b", text_node.text)
+        case (TextType.ITALIC.value):
+            return LeafNode("i", text_node.text)
+        case (TextType.CODE.value):
+            return LeafNode("code", text_node.text)
+        case (TextType.LINK.value):
+            return LeafNode("a", text_node.text, {"href": f"{text_node.url}"})
+        case (TextType.IMAGE.value):
+            return LeafNode("img", "", {"src": f"{text_node.url}", f"alt": f"{text_node.text}"})
+        case _:
+            raise ValueError(f"Invalid text type: {text_node.text_type}")
+        
+    pass
