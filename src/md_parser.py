18508d20f6693b78cb1e674df73c9ebe30b8783d
abanob-shehata <abanob.sheh@gmail.com>
2024-11-01 10:41:41 +0900

    add image and link split

--- src/md_parser.py
+++ src/md_parser.py
@@ -60,4 +60,77 @@ def extract_markdown_images(text):
 
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\[\]]*)\)", text)
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        images = extract_markdown_images(node.text)
+        if len(images) == 0:
+            new_nodes.append(node)
+            continue
+        split_text = split_add_image(node.text, images)
+        #print(split_text)
+        i = 0
+        for text in split_text:
+            if text == "":
+                continue
+            if text == "IMAGE_TO_INSERT":
+                new_nodes.append(TextNode(images[i][0], TextType.IMAGE, images[i][1]))
+                i += 1
+                continue
+            new_nodes.append(TextNode(text, TextType.TEXT))
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        links = extract_markdown_links(node.text)
+        if len(links) == 0:
+            new_nodes.append(node)
+            continue
+        split_text = split_add_link(node.text, links)
+        #print(split_text)
+        i = 0
+        for text in split_text:
+            if text == "":
+                continue
+            if text == "LINK_TO_INSERT":
+                new_nodes.append(TextNode(links[i][0], TextType.LINK, links[i][1]))
+                i += 1
+                continue
+            new_nodes.append(TextNode(text, TextType.TEXT))
+    return new_nodes
     
+def split_add_image(text, images):
+    if f"![{images[0][0]}]({images[0][1]})" not in text:
+        raise Exception("something went wrong with image split")
+    split_text = text.split(f"![{images[0][0]}]({images[0][1]})", 1)
+    #print(split_text)
+    if len(images) > 1:
+        return [split_text[0], "IMAGE_TO_INSERT"] + split_add_image(split_text[1], images[1:])
+    return [split_text[0], "IMAGE_TO_INSERT", split_text[1]]
+    
+def split_add_link(text, links):
+    if f"[{links[0][0]}]({links[0][1]})" not in text:
+        raise Exception("something went wrong with image split")
+    split_text = text.split(f"[{links[0][0]}]({links[0][1]})", 1)
+    #print(split_text)
+    if len(links) > 1:
+        return [split_text[0], "LINK_TO_INSERT"] + split_add_link(split_text[1], links[1:])
+    return [split_text[0], "LINK_TO_INSERT", split_text[1]]
+
+# def main():
+#     node = TextNode(
+#         "This is text with an image ![to boot dev](https://www.boot.dev) and ![to youtube](https://www.youtube.com/@bootdotdev)",
+#         TextType.TEXT,
+#     )
+#     node2 = TextNode(
+#         "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+#         TextType.TEXT,
+#     )
+#     new_nodes = split_nodes_image([node2])
+#     new_nodes2 = split_nodes_link([node2])
+#     print(new_nodes)
+#     print(new_nodes2)
+
+# main()
